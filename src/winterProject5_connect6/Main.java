384c837d629e4ffb5117d0d6f078600700a37967
leeejjju <dlwlswn02027@gmail.com>
2022-01-06 21:57:13 +0900

    1/6
    ㅂㅈㄷㄱㅂㅈㄷ

--- src/winterProject5_connect6/Main.java
+++ src/winterProject5_connect6/Main.java
@@ -2,8 +2,7 @@ package winterProject5_connect6;
 
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 
 /*
  *정보
@@ -13,10 +12,10 @@ import java.awt.event.MouseEvent;
  with 김휘진
  */
 
-
 public class Main extends JFrame {
+   private static final long serialVersionUID = 1L;
 
-   // 자동실행, 바둑판 그리기
+// 자동실행, 바둑판 그리기
    @Override // 그냥...실행되는놈임
    public void paint(Graphics g) {
       super.paint(g);
@@ -623,38 +622,33 @@ class alphago {
    // 가중치 설정을 위한 배열
    static int[][] weight = new int[19][19]; //일반가중치(계속 누적)
    static int[][] superWeight = new int[19][19]; //특수가중치(매 분석마다 리셋후 시작)
-   
-
 
    // 가중치 기본 누적하기 (매 실행 후에)
    public static void addWeight(int x, int y) {
-      int n = 1;
-
-      if (PlayBoard.playBoard[x][y] == 1)
-         n = 2;
-      else if (PlayBoard.playBoard[x][y] == 2)
-         n = 1;
-      else if (PlayBoard.playBoard[x][y] == 5) {
+      int n = 1; //누적할 가중치의 양. 내 돌인지 상대 돌인지에 따라 달라짐. 
+      
+      if (PlayBoard.playBoard[x][y] == 1) n = 2;
+      else if (PlayBoard.playBoard[x][y] == 2) n = 1;
+      else if (PlayBoard.playBoard[x][y] == 5) { //중립구 취급
          weight[x][y] = -1;
          superWeight[x][y] = -1;
          return;
-      } else
-         return;
-
+      } else  return; //이미 놓여진곳 취급 
+      
+      //팔방을 뒤져서 이미 놓여진 곳만 아니라면 가중치 누적. 
       for (int i = x - 1; i < x + 2; i++) {
          for (int j = y - 1; j < y + 2; j++) {
-            if (i == x && j == y) {
+            if (i == x && j == y) { //본인자리에는 -1 
                weight[x][y] = -1;
                superWeight[x][y] = -1;
             } else {
                try {
-                  if (PlayBoard.playBoard[i][j] == 0)
-                     weight[i][j] += n;
-               } catch (ArrayIndexOutOfBoundsException e) {
-               }
+                  if (PlayBoard.playBoard[i][j] == 0) weight[i][j] += n;
+               } catch (ArrayIndexOutOfBoundsException e) {} //인덱스 넘어서면 무시 
             }
          }
       }
+      
    }
 
 
@@ -662,19 +656,17 @@ class alphago {
    //판 읽고 특수가중치 누적하기 
    public static void addSuperWeight() {
 	   
-	   //초기화 
+	   //특수가중치 판 초기화 
 	   for(int i = 0; i < 19; i++) {
 		   for(int j = 0; j < 19; j++) {
 			   superWeight[i][j] = 0;
 		   }
 	   }
 
-	      int myCount;
-	      int add = 0;
+	   int myCount;
+	   int add = 0; //현재 가중치 몇번 누적했는지(두번 넘어가면 그만찾고 리턴. 어차피 한 턴에 두번밖에 못 두니까..)
 	     
-
-	      //// 놓으면 이길 때
-	      //// -------------------------------------------------------------------------------------
+	   //// 놓으면 이길 때(한방승리) -------------------------------------------------------------------------------------
 	      
 	      if(add >= 2) return;
 	      // 5 세로 공격
@@ -686,20 +678,15 @@ class alphago {
 	                  myCount++;
 	                  if (myCount == 5) { // 양끝 중 아무 빈곳에 가중치 왕창 이벤트
 	                     if (PlayBoard.playBoard[i][j - 5] == 0) {
-	                    	 superWeight[i][j - 5] += 80;
+	                    	superWeight[i][j - 5] += 80;
 	                        add++;
 	                     } else if (PlayBoard.playBoard[i][j + 1] == 0) {
-	                    	 superWeight[i][j + 1] += 80;
+	                    	superWeight[i][j + 1] += 80;
 	                        add++;
 	                     }
 	                  }
-	               } else {
-	                  myCount = 0;
-	               }
-	            } catch (ArrayIndexOutOfBoundsException e) {
-
-	            }
-
+	               } else myCount = 0;
+	            } catch (ArrayIndexOutOfBoundsException e) {}
 	         }
 	      }
 	      
@@ -732,11 +719,8 @@ class alphago {
 	                        }
 	                     }
 	                  }
-	               } else {
-	                  myCount = 0;
-	               }
-	            } catch (ArrayIndexOutOfBoundsException e) {
-	            }
+	               } else myCount = 0;
+	            } catch (ArrayIndexOutOfBoundsException e) {}
 	         }
 	      }
 	      
@@ -766,15 +750,11 @@ class alphago {
 	                        	superWeight[i ][j- 5] += 40; add++;}
 	                        }
 	                  }
-	               } else {
-	                  myCount = 0;
-	               }
-	            } catch (ArrayIndexOutOfBoundsException e) {
-
-	            }
-
+	               } else myCount = 0;
+	            } catch (ArrayIndexOutOfBoundsException e) {}
 	         }
 	      }
+	      
 	      
 	      if(add >= 2) return;
 	         // 2 (공백2) 2 공격 세로
@@ -788,7 +768,6 @@ class alphago {
 	                         if (PlayBoard.playBoard[i][j + 1] == 0 && PlayBoard.playBoard[i][j + 2] == 0 && PlayBoard.playBoard[i][j + 3] == Main.UserC && PlayBoard.playBoard[i][j + 4] == Main.UserC) {
 	                            superWeight[i][j + 1] += 40; add++;
 	                            superWeight[i][j + 2] += 40; add++;
-	                
 	                         } 
 	                         else if (PlayBoard.playBoard[i][j - 2] == 0 && PlayBoard.playBoard[i][j - 3] == 0 && PlayBoard.playBoard[i][j - 4] == Main.UserC && PlayBoard.playBoard[i][j - 5] == Main.UserC) {
 	                            superWeight[i][j - 2] += 40; add++;
@@ -846,13 +825,8 @@ class alphago {
 	                        add++;
 	                     }
 	                  }
-	               } else {
-	                  myCount = 0;
-	               }
-	            } catch (ArrayIndexOutOfBoundsException e) {
-
-	            }
-
+	               } else myCount = 0;
+	            } catch (ArrayIndexOutOfBoundsException e) {}
 	         }
 	      }
 	      
@@ -885,13 +859,8 @@ class alphago {
 	                        }
 	                     }
 	                  }
-	               } else {
-	                  myCount = 0;
-	               }
-	            } catch (ArrayIndexOutOfBoundsException e) {
-
-	            }
-
+	               } else myCount = 0;
+	            } catch (ArrayIndexOutOfBoundsException e) {}
 	         }
 	      }
 	      
@@ -981,9 +950,9 @@ class alphago {
 	      
 	      if(add >= 2) return;
 	   // 5 왼쪽 위에서 오른쪽 아래(좌대각\) 공격 
-	      for (int i = 0; i < 15; i++) {
+	      for (int i = 0; i < 19; i++) {
 	         myCount = 0;
-	         for (int j = 0; j < 15; j++) {
+	         for (int j = 0; j < 19; j++) {
 	            int temp1 = i;
 	            int temp2 = j;
 	            for (int k = 0; k < 5; k++) {
@@ -1023,9 +992,9 @@ class alphago {
 	      
 	      if(add >= 2) return;
 	      // 4 왼쪽 위에서 오른쪽 아래(좌대각\) 공격
-	      for (int i = 0; i < 15; i++) {
+	      for (int i = 0; i < 19; i++) {
 	         myCount = 0;
-	         for (int j = 0; j < 15; j++) {
+	         for (int j = 0; j < 19; j++) {
 	            int temp1 = i;
 	            int temp2 = j;
 	            for (int k = 0; k < 4; k++) {
@@ -1067,9 +1036,9 @@ class alphago {
 
 		   if(add >= 2) return;
 		   // 3(좌대각\) 1공백 공격
-		   for(int i = 0;i<15;i++){
+		   for(int i = 0;i<19;i++){
 		      myCount = 0;
-		      for (int j = 0; j < 15; j++) {
+		      for (int j = 0; j < 19; j++) {
 		         int temp1 = i;
 		         int temp2 = j;
 		         for (int k = 0; k < 3; k++) {
@@ -1156,15 +1125,13 @@ class alphago {
 		              }
 		          }
 		       
-		   
-		   
-	      
+
 	      
 	      if(add >= 2) return;
 	   // 5 오른쪽위에서 왼쪽아래 (우대각/) 공격 
-	      for (int j = 0; j < 15; j++) {
+	      for (int j = 0; j < 19; j++) {
 	         myCount = 0;
-	         for (int i = 5; i < 15; i++) {
+	         for (int i = 5; i < 19; i++) {
 	            int temp1 = i;
 	            int temp2 = j;
 	            for (int k = 0; k < 5; k++) {
@@ -1200,9 +1167,9 @@ class alphago {
 	      
 	      if(add >= 2) return;
 	      // 4 오른쪽위에서 왼쪽아래 (우대각/) 공격
-	      for (int j = 0; j < 15; j++) {
+	      for (int j = 0; j < 19; j++) {
 	         myCount = 0;
-	         for (int i = 5; i < 15; i++) {
+	         for (int i = 5; i < 19; i++) {
 	            int temp1 = i;
 	            int temp2 = j;
 	            for (int k = 0; k < 4; k++) {
@@ -1244,9 +1211,9 @@ class alphago {
 
 		   if(add >= 2) return;
 		   // 3(우대각/) 1공백 공격
-		   for(int j = 0;j<15;j++){
+		   for(int j = 0;j<19;j++){
 		      myCount = 0;
-		      for (int i = 0; i < 15; i++) {
+		      for (int i = 0; i < 19; i++) {
 		         int temp1 = i;
 		         int temp2 = j;
 		         for (int k = 0; k < 3; k++) {
@@ -1330,8 +1297,7 @@ class alphago {
 		                            } catch (ArrayIndexOutOfBoundsException e) {}
 		                            temp1--;
 		                            temp2++;
-		                         } else
-		                            myCount = 0;
+		                         } else myCount = 0;
 		                 }
 		                 catch(ArrayIndexOutOfBoundsException e) {}
 		              }
@@ -1483,9 +1449,7 @@ class alphago {
 	                            superWeight[i][j + 1] += 40; add++;
 	                         }
 	                      } 
-	                   }else {
-	                      myCount = 0;
-	                   }
+	                   }else myCount = 0;
 	                } catch (ArrayIndexOutOfBoundsException e) {}
 	             }
 	          }
@@ -1629,13 +1593,8 @@ class alphago {
 	                            superWeight[i + 1][j] += 40; add++;
 	                         }
 	                      } 
-	                   }else {
-	                      myCount = 0;
-	                   }
-	                } catch (ArrayIndexOutOfBoundsException e) {
-
-	                }
-
+	                   }else myCount = 0;
+	                } catch (ArrayIndexOutOfBoundsException e) {}
 	             }
 	          }
 	      
@@ -1644,9 +1603,9 @@ class alphago {
 	      
 	      if(add >= 2) return;
 	  	// 5 왼쪽 위에서 오른쪽 아래(좌대각\) 방어
-	  	   for(int i = 0;i<15;i++){
+	  	   for(int i = 0;i<19;i++){
 	  	      myCount = 0;
-	  	      for (int j = 0; j < 15; j++) {
+	  	      for (int j = 0; j < 19; j++) {
 	  	         int temp1 = i;
 	  	         int temp2 = j;
 	  	         for (int k = 0; k < 5; k++) {
@@ -1682,9 +1641,9 @@ class alphago {
 
 	      if(add >= 2) return;
 	   // 4 왼쪽 위에서 오른쪽 아래(좌대각\) 방어
-	   for(int i = 0;i<15;i++){
+	   for(int i = 0;i<19;i++){
 	      myCount = 0;
-	      for (int j = 0; j < 15; j++) {
+	      for (int j = 0; j < 19; j++) {
 	         int temp1 = i;
 	         int temp2 = j;
 	         for (int k = 0; k < 4; k++) {
@@ -1726,9 +1685,9 @@ class alphago {
 
 	   if(add >= 2) return;
 	   // 3(좌대각\) 1공백 방어
-	   for(int i = 0;i<15;i++){
+	   for(int i = 0;i<19;i++){
 	      myCount = 0;
-	      for (int j = 0; j < 15; j++) {
+	      for (int j = 0; j < 19; j++) {
 	         int temp1 = i;
 	         int temp2 = j;
 	         for (int k = 0; k < 3; k++) {
@@ -1806,22 +1765,13 @@ class alphago {
 	                                          superWeight[temp1 - 2][temp2 - 2] += 40; add++;
 	                                    }
 	                               }
-	                               
-	                            } catch (ArrayIndexOutOfBoundsException e) {
-
-	                            }
+	                            } catch (ArrayIndexOutOfBoundsException e) {}
 	                            temp1++;
 	                            temp2++;
-	                         } else
-	                            myCount = 0;
-	                 }
-	                 catch(ArrayIndexOutOfBoundsException e) {
-	                    
-	                 }
-	             
+	                         } else myCount = 0;
+	                 }catch(ArrayIndexOutOfBoundsException e) {}
 	              }
 	              }
-
 	          }
 	       
 	   
@@ -1829,9 +1779,9 @@ class alphago {
 
 	   if(add >= 2) return;
 	// 5 오른쪽위에서 왼쪽아래(우대각/) 방어
-	   for( int j = 0;j<15;j++){
+	   for( int j = 0;j<19;j++){
 	      myCount = 0;
-	      for (int i = 5; i < 15; i++) {
+	      for (int i = 5; i < 19; i++) {
 	         int temp1 = i;
 	         int temp2 = j;
 	         for (int k = 0; k < 5; k++) {
@@ -1864,9 +1814,9 @@ class alphago {
 	   
 	   if(add >= 2) return;
 	   // 4 오른쪽위에서 왼쪽아래(우대각/) 방어
-	   for( int j = 0;j<15;j++){
+	   for( int j = 0;j<19;j++){
 	      myCount = 0;
-	      for (int i = 5; i < 15; i++) {
+	      for (int i = 5; i < 19; i++) {
 	         int temp1 = i;
 	         int temp2 = j;
 	         for (int k = 0; k < 4; k++) {
@@ -1907,9 +1857,9 @@ class alphago {
 	   
 	   if(add >= 2) return;
 	   // 3(우대각/) 1공백 방어
-	   for(int j = 0;j<15;j++){
+	   for(int j = 0;j<19;j++){
 	      myCount = 0;
-	      for (int i = 0; i < 15; i++) {
+	      for (int i = 0; i < 19; i++) {
 	         int temp1 = i;
 	         int temp2 = j;
 	         for (int k = 0; k < 3; k++) {
@@ -1940,6 +1890,7 @@ class alphago {
 	         }
 	      }
 	   }
+	      
 	   
 	   if(add >= 2) return;
 	      // 2 (공백2) 2 방어 (우대각/)오른쪽 위에서 왼쪽 아래 
@@ -1992,27 +1943,17 @@ class alphago {
 	                                         superWeight[temp1 + 2][temp2 - 2] += 40; add++;
 	                                   }
 	                               }
-	                               
-	                            } catch (ArrayIndexOutOfBoundsException e) {
-
-	                            }
+	                            } catch (ArrayIndexOutOfBoundsException e) {}
 	                            temp1--;
 	                            temp2++;
-	                         } else
-	                            myCount = 0;
-	                 }
-	                 catch(ArrayIndexOutOfBoundsException e) {
-	                    
-	                 }
-	             
+	                         } else myCount = 0;
+	                 }catch(ArrayIndexOutOfBoundsException e) {}
 	              }
 	              }
-
 	          }
 	   
 	   
-	   
-	   
+
 
 	   if(add >= 2) return;
 	   
@@ -2033,7 +1974,6 @@ class alphago {
    
 
    
-   
    // 일반가중치+특수가중치 판에서 최대 가중치를 찾아 x,y 값 저장해주기
    public static void returnPoint(int[][] board) {
 	   
@@ -2050,8 +1990,7 @@ class alphago {
 	   
    }
    
-   
-   
+    
    // 현재 가중치 상태 콘솔에 출력
    public static void showWeight() {
       for (int i = 0; i < 19; i++) {
@@ -2063,4 +2002,5 @@ class alphago {
       System.out.println("");
    }
 
+   
 }
